dde52fcadbd965e54f73a3a70d2360b42f3f050f
Anggara Setiawan <aanggaadc@gmail.com>
2022-07-29 22:27:18 +0700

    config global for axios

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom'
-import {store} from '../src/store/store'
-import {Provider} from 'react-redux'
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "../src/store/store";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import Axios from "axios";
+Axios.defaults.baseURL = "https://jsonplaceholder.typicode.com/";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <App />
-      </Provider>     
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
